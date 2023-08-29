e4a0c9b46b4faa36e042f73d9d4e950bb1658348
Hennan Lewis <hennan.lewis@gmail.com>
2023-08-29 14:02:19 -0300

    Dynamic selection and convertion of DNG files

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import rawpy
 from PIL import Image
+import os
+import glob
 
-# Carregando a imagem DNG
-dng_path = "DSC_2298.dng"
-raw = rawpy.imread(dng_path)
+pasta = "."  # Substitua pelo caminho da pasta que você deseja verificar
 
-# Convertendo a imagem RAW para uma imagem RGB
-rgb = raw.postprocess()
+# Use a função glob para pegar todos os arquivos .dng na pasta
+arquivos_dng = glob.glob(os.path.join(pasta, "*.dng"))
 
-# Convertendo a imagem RGB para o formato JPEG
-jpeg_image = Image.fromarray(rgb)
-jpeg_path = "DSC_2298.dng.jpg"
-jpeg_image.save(jpeg_path)
+# Imprima o nome de cada arquivo .dng
+for arquivo_dng in arquivos_dng:
+	nome_arquivo = os.path.basename(arquivo_dng)
 
-print("Imagem DNG convertida para JPEG e salva com sucesso.")
+	# Carregando a imagem DNG
+	raw = rawpy.imread(nome_arquivo)
+
+	# Convertendo a imagem RAW para uma imagem RGB
+	rgb = raw.postprocess()
+
+	# Convertendo a imagem RGB para o formato JPEG
+	jpeg_image = Image.fromarray(rgb)
+	jpeg_path = f"output/{nome_arquivo}.jpg"
+	jpeg_image.save(jpeg_path)
+
+	print(f"Imagem {nome_arquivo} convertida para JPEG e salva com sucesso.")
